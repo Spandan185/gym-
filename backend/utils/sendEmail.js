4b383628709f84667bbcc0f5e3414ffa57c29783
spandan185 <spandannag18@gmail.com>
2025-06-15 13:35:22 +0530

    gym react js first commit

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,23 @@
+import nodeMailer from "nodemailer";
+
+export const sendEmail = async(Options)=>{
+    const transporter = nodeMailer.createTransport({
+        
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  service:process.env.SMTP_SERVICE,
+  auth: {
+    user: process.env.SMTP_MAIL,
+    pass: process.env.SMTP_PASSWORD,
+  },
+    });
+const mailoptions = {
+    from :process.env.SMTP_MAIL,
+    to: Options.email,
+    subject: Options.subject,
+    text :`${Options.message} \n\nEmail of user who sent The Message ${Options.userEmail}`,
+};
+
+await transporter.sendMail(mailoptions);
+
+};
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        Sweat ,Sacrifice , Success
+    </footer>
+  )
+}
+
+export default Footer
